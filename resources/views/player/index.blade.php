ac95e153441db8d6ee8d9c1aa978accb8aeecf95
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2020-07-27 11:13:59 -0500

    Players List layout update

--- resources/views/player/index.blade.php
+++ resources/views/player/index.blade.php
@@ -11,7 +11,14 @@
 <body>
     <div class="min-h-screen flex items-center justify-center flex-col items-stretch bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         @foreach($players as $p)
-            <a href="/player/{{ $p->id }}" class="block px-4 py-2 my-3 text-center border border-transparent text-2xl leading-5 font-medium text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:shadow-outline-green focus:border-green-700 active:bg-green-700 transition duration-150 ease-in-out">{{ $p->first_name }}</a>
+            <div class="player mb-5">
+                <p class="mb-2 text-2xl">{{ $p->first_name }}</p>
+                <div class="option grid grid-cols-4 gap-2">
+                    <a href="/player/{{ $p->id }}/bat" class="col-span-2 px-4 py-2 text-center border border-transparent text-sm leading-5 font-medium text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:shadow-outline-green focus:border-green-700 active:bg-green-700 transition duration-150 ease-in-out">Batting</a>
+                    <a href="/player/{{ $p->id }}/pitch" class="col-span-2 px-4 py-2 text-center border border-transparent text-sm leading-5 font-medium text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:shadow-outline-green focus:border-green-700 active:bg-green-700 transition duration-150 ease-in-out">Pitching</a>
+                    <a href="/player/{{ $p->id }}" class="col-span-4 px-4 py-2 text-center border border-transparent text-sm leading-5 font-medium text-white bg-gray-600 hover:bg-green-500 focus:outline-none focus:shadow-outline-green focus:border-green-700 active:bg-green-700 transition duration-150 ease-in-out">Profile</a>
+                </div>
+            </div>
         @endforeach
     </div>
 </body>
